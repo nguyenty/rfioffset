8e21cda380196755f5a5e4784babd7575c9426ce
Yet Nguyen <tienyettoan@gmail.com>
2015-02-20 15:48:51 -0600

    ss

--- runQLfit_prc.R
+++ runQLfit_prc.R
@@ -11,14 +11,12 @@ u <- svd(full_model)$u
 colnames(u) <- paste("Prc", 1:10, sep = "")
 for (i in 1:dim(u)[2]) assign(paste("Prc", i, sep =''), u[,i])
 vnf <- c("Line", "Block", paste("Prc", 1:10, sep = ''))
-vnf <- c("Line", "Block")
 vntest <- vnf
 
-fit_model(vnf, vntest, criteria,  counts, model_th)
 criteria <- 1
 model_th <- 1
 repeat{
-out_model <- fit_model(vnf, vntest, criteria, counts, model_th)
+out_model <- fit_model(vnf, vntest, criteria, model_th)
 #out_model <- s[[i]]
 assign(paste("ms_criteria", model_th, sep = "_" ),out_model)
 ms_val <- get(paste("ms_criteria", model_th, sep = "_" ))
@@ -28,28 +26,8 @@ cov_set <- list_model(vnf, vntest)$test.mat # dim(cov_set)
 res <- data.frame(criteria = colnames(ms_val)[criteria], 
                   model = model_th, 
                   cov_del = rownames(cov_set)[ cov_del])
-out_pairedend_cbc <- rbind(out_pairedend_cbc, res)
 if (cov_del ==1) break
-block_ind <- grep("Block2", colnames(full_model))
-blockorder_ind <-grep("Blockorder", colnames(full_model))
-indicator <- FALSE
-if(length(block_ind)!=0){
-  if(cov_del+1 == cov_set["Block",2])
-  {
-    full_model <- full_model[, -c(block_ind, block_ind+1, block_ind+2)]
-    indicator <- TRUE
-  }
-}
-
-if(length(blockorder_ind)!=0){
-  if(cov_del+1 == cov_set["Blockorder", 2])
-  {
-    full_model <- full_model[, -blockorder_ind] 
-    indicator <- TRUE
-  }
-}
-
-if (indicator == FALSE) full_model <- full_model[, -(cov_del +1)] 
+vnf <- vntest <- vnf[-cov_del]
   model_th <- model_th +1
 }
 
